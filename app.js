c041a362bcf7963df6268008ebf9ab95b51ae591
Travis Tatro <travistatro@gmail.com>
2016-10-04 21:11:30 +0000

    header/footer stylesheets and inital layout

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require("express"),
 
 app.set("view engine", "ejs");
 
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res){
     res.render("index");    
 });
