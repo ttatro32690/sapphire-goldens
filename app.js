7717f571a8812a622b550fb4c65aebcda0c94d93
Travis Tatro <travistatro@gmail.com>
2016-10-05 20:43:37 +0000

    working routes

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ app.set("view engine", "ejs");
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
-var indexRoutes = require("./routes/index");
-app.use("/", indexRoutes);
+var indexRoutes = require("./routes/index"),
+    aboutRoutes = require("./routes/about");
+    
+app.use(indexRoutes);
+app.use("/about", aboutRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Server is Listening"); 
