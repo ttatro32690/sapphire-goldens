f05d69119ff5948a0d3db1cef054687993dc8561
Travis Tatro <travistatro@gmail.com>
2016-10-05 20:28:39 +0000

    initial index routes

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ app.set("view engine", "ejs");
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res){
-    res.render("index");    
-});
+var indexRoutes = require("./routes/index");
+app.use("/", indexRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Server is Listening"); 
