0ae21f23bd74895bd522f71dccdb0b2f2dcb4dc1
Travis Tatro <travistatro@gmail.com>
2016-10-05 20:55:03 +0000

    beginning of restful routing

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
 var indexRoutes = require("./routes/index"),
-    aboutRoutes = require("./routes/about");
+    aboutRoutes = require("./routes/about"),
+    contactRoutes = require("./routes/contact"),
+    galleryRoutes = require("./routes/gallery"),
+    goldenRoutes = require("./routes/goldens");
     
 app.use(indexRoutes);
 app.use("/about", aboutRoutes);
+app.use("/contact", contactRoutes);
+app.use("/gallery", galleryRoutes);
+app.use("/goldens", goldenRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Server is Listening"); 
