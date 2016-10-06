9677c5d9ef371721683cb6d37a6342c4defe9569
Travis Tatro <travistatro@gmail.com>
2016-10-06 23:37:30 +0000

    favicon logic for browser

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express"),
+    favicon = require("serve-favicon"),
         app = express();
 
 app.set("view engine", "ejs");
 
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + "/public/images/favicon.ico")); 
 
 app.get("/", function(req, res){
     res.render("index");
