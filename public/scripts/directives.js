fa0d429d5f4481068a34415f0002333595755563
Travis Tatro <travistatro@gmail.com>
2017-01-17 13:54:54 +0000

    sticky landing page footer

--- public/scripts/directives.js
+++ public/scripts/directives.js
@@ -7,12 +7,23 @@ goldenApp.directive('header', function(){
    };
 });
 
-goldenApp.directive('footer', function(){
+goldenApp.directive('footer', ['$rootScope', function($rootScope){
    return{
       templateUrl: '/views/templates/footer.ejs',
       replace: true,
+      link: function(scope, element, attrs){
+         $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams, options){
+            console.log(toState.name);
+            
+            if(toState.name == 'landing'){
+               element.addClass('navbar-fixed-bottom');
+            } else {
+               element.removeClass('navbar-fixed-bottom');
+            }
+         });
+      }
    };
-});
+}]);
 
 goldenApp.directive('message', function(){
    return{
