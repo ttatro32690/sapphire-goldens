a2c8560bf05ce264cf8d2c853939a949d3d08d76
Travis Tatro <travistatro@gmail.com>
2017-01-18 22:08:01 +0000

    updated our story image and our story data temp seeds

--- app.js
+++ app.js
@@ -63,6 +63,9 @@ app.get('*', function(req, res){
     }
 });
 
+var seeds = require('./seeds');
+seeds();
+
 // Server Listen Command
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log('Server is Listening'); 
--- seeds.js
+++ seeds.js
@@ -10,7 +10,7 @@ var sapphireData = {
     about: "/public/images/about.png",
     story: ["We are the Kauranen family from Southeastern MA. Other than the love that we share for one another, we share a common passion for our Golden Retrievers.",
         "Our 1st Golden put us under 'The Golden Spell'. Her name was Shadow and is 13 yrs. old in this picture with us. We spend much of our time and energy making sure the 8 Goldens that own us, are happy and healthy.",
-        "Our first litter of puppies was born 20 yrs. ago and historically Sapphire Goldens produce 4 litters per year. Our girls are from champion pedigrees and although we have always used Champions from local Show Kennels, we are hoping that we will have our own champions someday. All have hip, heart and eye clearances.",
+        "Our first litter of puppies was born in 1994 and historically Sapphire Goldens produce 4 litters per year. Our girls are from champion pedigrees and although we have always used Champions from local Show Kennels, we are hoping that we will have our own champions someday. All have hip, elbow, heart and eye clearances.",
         "Our objective is to raise Golden Retrievers that are healthy, beautiful to the breed standard, and have that 'I want to be by your side' personality.",
         "Linda, Steven & Kerri"
     ]
@@ -30,28 +30,28 @@ function seedDB() {
         }
     });
     
-    Goldens.remove({}, function(err){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("Removed");
-            for(var i = 1; i < 39; i++){
+    // Goldens.remove({}, function(err){
+    //     if(err){
+    //         console.log(err);
+    //     } else {
+    //         console.log("Removed");
+    //         for(var i = 1; i < 39; i++){
                 
-                var newGolden = {
-                    name: "sapphire" + i,
-                    url: "/public/images/sapphire" + i + ".png"
-                };
+    //             var newGolden = {
+    //                 name: "sapphire" + i,
+    //                 url: "/public/images/sapphire" + i + ".png"
+    //             };
                 
-                Goldens.create(newGolden, function(err, createdGolden){
-                    if(err){
-                        console.log(err);
-                    } else {
-                        console.log(createdGolden);
-                    }
-                });
-            }
-        }
-    });
+    //             Goldens.create(newGolden, function(err, createdGolden){
+    //                 if(err){
+    //                     console.log(err);
+    //                 } else {
+    //                     console.log(createdGolden);
+    //                 }
+    //             });
+    //         }
+    //     }
+    // });
 }
 
 module.exports = seedDB;
