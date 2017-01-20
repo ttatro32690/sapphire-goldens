ccace216aeeb6a2fda24939064e0d2ab05d2b154
Travis Tatro <travistatro@gmail.com>
2017-01-20 13:46:59 +0000

    seed sapphire temporary update

--- seeds.js
+++ seeds.js
@@ -9,7 +9,7 @@ var sapphireData = {
     ygrc: "/public/images/ygrclogo.png",
     about: "/public/images/about.png",
     story: ["We are the Kauranen family from Southeastern MA. Other than the love that we share for one another, we share a common passion for our Golden Retrievers.",
-        "Our 1st Golden put us under 'The Golden Spell'. Her name was Shadow and is 13 yrs. old in this picture with us. We spend much of our time and energy making sure the 8 Goldens that own us, are happy and healthy.",
+        "Our 1st Golden put us under 'The Golden Spell'. Her name was Shadow and is 13 yrs. old in this picture. We spend much of our time and energy making sure the 8 Goldens that own us, are happy and healthy.",
         "Our first litter of puppies was born in 1994 and historically Sapphire Goldens produce 4 litters per year. Our girls are from champion pedigrees and although we have always used Champions from local Show Kennels, we are hoping that we will have our own champions someday. All have hip, elbow, heart and eye clearances.",
         "Our objective is to raise Golden Retrievers that are healthy, beautiful to the breed standard, and have that 'I want to be by your side' personality.",
         "Linda, Steven & Kerri"
