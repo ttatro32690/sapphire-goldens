bc1ab7a1dda1ce6fb232f1488b89cdac4e27add7
Travis Tatro <travistatro@gmail.com>
2017-05-31 15:17:07 +0000

    updated application and non-refundable

--- public/scripts/application/controllers.js
+++ public/scripts/application/controllers.js
@@ -15,6 +15,13 @@ goldenApp.controller('applicationNewController', ['$scope', '$state', '$window',
     $scope.new = true;
     $scope.application = application;
     
+    $scope.states = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC',
+                    'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
+                    'ME', 'MD', 'MH', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE',
+                    'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR',
+                    'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
+                    'WV', 'WI', 'WY'];
+    
     $scope.saveApplication = function(){
         if($scope.appl.$error.required == null){
             ApplicationFunctions.saveApplication($scope.application, $state.current.parent);
@@ -26,6 +33,13 @@ goldenApp.controller('applicationNewController', ['$scope', '$state', '$window',
 goldenApp.controller('applicationEditController', ['$scope', '$state', 'application', 'ApplicationFunctions', function($scope, $state, application, ApplicationFunctions){
     $scope.edit = true;
     $scope.application = application;
+    
+    $scope.states = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC',
+                    'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
+                    'ME', 'MD', 'MH', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE',
+                    'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR',
+                    'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
+                    'WV', 'WI', 'WY'];
     
     $scope.updateApplication = function(){
         if($scope.appl.$error.required == null){
